26c55a7e961c6201b9b01bf12a9458cb7e873a54
lazio14 <laziofly@yeah.net>
2014-11-08 10:03:00 +0800

    创建WebClient的工程。

---
+++ WebClient/WebClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebClient
+//
+//  Created by lazio14 on 14/11/8.
+//  Copyright (c) 2014年 lazio14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
