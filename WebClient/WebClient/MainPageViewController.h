77e6f50b3d05f41e0fa963878379046ba9ad40ba
lazio14 <laziofly@yeah.net>
2014-11-08 10:40:00 +0800

    获取到网页的内容。

--- WebClient/WebClient/MainPageViewController.h
+++ WebClient/WebClient/MainPageViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainPageViewController.h
 //  WebClient
 //
 //  Created by lazio14 on 14/11/8.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainPageViewController : UITableViewController
 
 @end
-
